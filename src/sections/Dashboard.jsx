d15c23a48a27bf3c7e38c255260d6658142e8c31
joscormir <joscormir@gmail.com>
2024-04-23 18:33:49 +0200

    Include icons for different info cards in dashboard. Modify Dashbaord return to include styles in Dashboard.module.scss

--- src/sections/Dashboard.jsx
+++ src/sections/Dashboard.jsx
@@ -1,7 +1,14 @@
 import { githubApiResponses } from '../github_api_responses'
+import styles from './Dashboard.module.scss'
 import Lock from '../assets/icons/lock.svg?react'
 import UnLock from '../assets/icons/unlock.svg?react'
-import styles from './Dashboard.module.scss'
+import Check from '../assets/icons/check.svg?react'
+import Error from '../assets/icons/error.svg?react'
+import PullRequest from '../assets/icons/git-pull-request.svg?react'
+import Star from '../assets/icons/star.svg?react'
+import Watcher from '../assets/icons/watchers.svg?react'
+import Fork from '../assets/icons/repo-forked.svg?react'
+import Issue from '../assets/icons/issue-opened.svg?react'
 
 /*async function onCLickgetRepo() {
   const octokit = new Octokit({
@@ -24,7 +31,9 @@ import styles from './Dashboard.module.scss'
 function isoDateFormat(date) {
   const dateFormatted = new Date(date)
   const today = new Date()
+
   if (today.getFullYear() - dateFormatted.getFullYear() > 0) {
+    console.log(today.getFullYear() - dateFormatted.getFullYear())
     return `${today.getFullYear() - dateFormatted.getFullYear()} years ago`
   } else if (today.getMonth() - dateFormatted.getMonth() > 0) {
     return `${today.getMonth() - dateFormatted.getMonth()} months ago`
@@ -57,17 +66,48 @@ export function Dashboard() {
               {repo.repositoryData.private ? <Lock /> : <UnLock />}
             </header>
 
-            <section>
-              <p>
-                Last update {isoDateFormat(repo.repositoryData.updated_at)}{' '}
+            <div className={styles.widget__body}>
+              <div className={styles.widget__status}>
+                <p>
+                  Last update {isoDateFormat(repo.repositoryData.updated_at)}{' '}
+                </p>
+                {repo.CiStatus.workflow_runs.length > 0 && (
+                  <div>
+                    {repo.CiStatus.workflow_runs[0].status === 'completed' ? (
+                      <Check />
+                    ) : (
+                      <Error />
+                    )}
+                  </div>
+                )}
+              </div>
+              <p className={styles.widget__description}>
+                {repo.repositoryData.description}
               </p>
-              {repo.CiStatus.workflow_runs.length > 0 && (
-                <div>
-                  {/*repo.CiStatus.workflow_runs[0].status === 'completed' ? <Check/> : <Error />*/}
-                </div>
-              )}
-            </section>
-            <footer></footer>
+            </div>
+
+            <footer className={styles.widget__footer}>
+              <div className={styles.widget__stat}>
+                <Star />
+                {repo.repositoryData.stargazers_count}
+              </div>
+              <div className={styles.widget__stat}>
+                <Watcher />
+                {repo.repositoryData.watchers_count}
+              </div>
+              <div className={styles.widget__stat}>
+                <PullRequest />
+                {repo.pullRequest.length}
+              </div>
+              <div className={styles.widget__stat}>
+                <Fork />
+                {repo.repositoryData.forks_count}
+              </div>
+              <div className={styles.widget__stat}>
+                <Issue />
+                {repo.repositoryData.open_issues_count}
+              </div>
+            </footer>
           </article>
         ))}
       </section>
