7faab34b9013072957788cebe1c925f42e3d5df0
joscormir <joscormir@gmail.com>
2024-04-30 11:40:33 +0200

    Fix a bug in isoDateFormat.js cause by no reading properly Date object documentation

--- src/services/isoDateFormat.js
+++ src/services/isoDateFormat.js
@@ -1,14 +1,16 @@
 export function isoDateFormat(date) {
     const dateFormatted = new Date(date)
     const today = new Date()
-  
+   
     if (today.getFullYear() - dateFormatted.getFullYear() > 0) {
       console.log(today.getFullYear() - dateFormatted.getFullYear())
       return `${today.getFullYear() - dateFormatted.getFullYear()} years ago`
     } else if (today.getMonth() - dateFormatted.getMonth() > 0) {
+      console.log(today.getMonth() - dateFormatted.getMonth() )
       return `${today.getMonth() - dateFormatted.getMonth()} months ago`
-    } else if (today.getDay() - dateFormatted.getDay() > 0) {
-      return `${today.getDay() - dateFormatted.getDay()} days ago`
+    } else if (today.getDate() - dateFormatted.getDate() > 0) {
+      console.log(today.getDate() - dateFormatted.getDate()  )
+      return `${today.getDate() - dateFormatted.getDay()} days ago`
     } else {
       return 'today'
     }
