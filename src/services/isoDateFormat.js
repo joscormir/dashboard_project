6bc15717a872a3d2f283678ec9abce966159b237
joscormir <joscormir@gmail.com>
2024-04-30 11:41:54 +0200

    Delete console.log for debugging in isoDateFormat.js

--- src/services/isoDateFormat.js
+++ src/services/isoDateFormat.js
@@ -1,17 +1,14 @@
 export function isoDateFormat(date) {
-    const dateFormatted = new Date(date)
-    const today = new Date()
-   
-    if (today.getFullYear() - dateFormatted.getFullYear() > 0) {
-      console.log(today.getFullYear() - dateFormatted.getFullYear())
-      return `${today.getFullYear() - dateFormatted.getFullYear()} years ago`
-    } else if (today.getMonth() - dateFormatted.getMonth() > 0) {
-      console.log(today.getMonth() - dateFormatted.getMonth() )
-      return `${today.getMonth() - dateFormatted.getMonth()} months ago`
-    } else if (today.getDate() - dateFormatted.getDate() > 0) {
-      console.log(today.getDate() - dateFormatted.getDate()  )
-      return `${today.getDate() - dateFormatted.getDay()} days ago`
-    } else {
-      return 'today'
-    }
+  const dateFormatted = new Date(date)
+  const today = new Date()
+
+  if (today.getFullYear() - dateFormatted.getFullYear() > 0) {
+    return `${today.getFullYear() - dateFormatted.getFullYear()} years ago`
+  } else if (today.getMonth() - dateFormatted.getMonth() > 0) {
+    return `${today.getMonth() - dateFormatted.getMonth()} months ago`
+  } else if (today.getDate() - dateFormatted.getDate() > 0) {
+    return `${today.getDate() - dateFormatted.getDay()} days ago`
+  } else {
+    return 'today'
   }
+}
